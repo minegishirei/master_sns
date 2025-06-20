b808076ab0ea7e85f4600fe966ccc79551adf7f3
rei.minegishi <rei.minegishi@gmail.com>
2025-06-20 17:09:39 +0900

    test

--- code/tweet/main.py
+++ code/tweet/main.py
@@ -39,20 +39,20 @@ client = tweepy.Client(
 
 
 
-# # ohmycat
-# #Twitter Developer Portalから取得したキーを設定
-# CONSUMER_KEY = 'SE1MVm9Ta2hRMGdJRmJaYlBhVVI6MTpjaQ'
-# CONSUMER_SECRET = 'Lbagx30xH1QSIRnEoZHkVC_9OXvyudi8WjMykuXYE5Eu2R6M6Z'
-# ACCESS_TOKEN = '1266718659671556102-UCUQte7FuhcvungNZovCBVBPyU4DgC'
-# ACCESS_SECRET = 'o6r4V2JD5DhH9gIKBw0va3yb89cBddrgYfV9stiYV7iol'
-# BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAO9c2gEAAAAAw772Bhonpi%2FyaCf1x10%2BogWwE9w%3D3hBfF1MmoKy7HxBPBPMojxA65IEkRLAZCwBUR19pYoTXhAo0Wn"
-# client = tweepy.Client(
-# 	consumer_key = CONSUMER_KEY,
-# 	consumer_secret = CONSUMER_SECRET,
-# 	access_token = ACCESS_TOKEN,
-# 	access_token_secret = ACCESS_SECRET,
-# 	#bearer_token = BEARER_TOKEN
-# )
+# ohmycat
+#Twitter Developer Portalから取得したキーを設定
+CONSUMER_KEY = 'SE1MVm9Ta2hRMGdJRmJaYlBhVVI6MTpjaQ'
+CONSUMER_SECRET = 'Lbagx30xH1QSIRnEoZHkVC_9OXvyudi8WjMykuXYE5Eu2R6M6Z'
+ACCESS_TOKEN = '1266718659671556102-UCUQte7FuhcvungNZovCBVBPyU4DgC'
+ACCESS_SECRET = 'o6r4V2JD5DhH9gIKBw0va3yb89cBddrgYfV9stiYV7iol'
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAO9c2gEAAAAAw772Bhonpi%2FyaCf1x10%2BogWwE9w%3D3hBfF1MmoKy7HxBPBPMojxA65IEkRLAZCwBUR19pYoTXhAo0Wn"
+client = tweepy.Client(
+	consumer_key = CONSUMER_KEY,
+	consumer_secret = CONSUMER_SECRET,
+	access_token = ACCESS_TOKEN,
+	access_token_secret = ACCESS_SECRET,
+	#bearer_token = BEARER_TOKEN
+)
 
 
 def create_tweet(content):
